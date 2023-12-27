0566a760e56cb716e589471d5dd967ab85b99585
Jimmy White <jimmyeao@users.noreply.github.com>
2023-12-27 17:29:58 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+using GongSolutions.Wpf.DragDrop;
+using System.Collections.ObjectModel;
+
+using System.Windows.Controls;
+using System.Windows.Input;
+using NAudio.Wave;
+using System.Collections.Generic;
+using System.Windows.Media;
+using GongSolutions.Wpf.DragDrop;
+
+namespace MP3Joiner
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new YourViewModel();
+        }
+
+
+        private void btnAddFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "MP3 Files|*.mp3"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                YourViewModel viewModel = DataContext as YourViewModel;
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    viewModel.FileList.Add(file);
+                }
+            }
+        }
+
+        private void btnJoinFiles_Click(object sender, RoutedEventArgs e)
+        {
+            // Call your method to join the files in fileList
+        }
+    }
+}
+
---
+++ YourViewModel.cs
@@ -0,0 +1,53 @@
+﻿// YourViewModel.cs
+using GongSolutions.Wpf.DragDrop;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace MP3Joiner
+{
+    public class YourViewModel
+    {
+        public ObservableCollection<string> FileList { get; set; }
+        public YourDropHandler DropHandler { get; private set; }
+
+        public YourViewModel()
+        {
+            FileList = new ObservableCollection<string>();
+            DropHandler = new YourDropHandler(this);
+        }
+    }
+
+    public class YourDropHandler : IDropTarget
+    {
+        private readonly YourViewModel _viewModel;
+
+        public YourDropHandler(YourViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public void DragOver(IDropInfo dropInfo)
+        {
+            // Logic for handling drag over event
+            dropInfo.Effects = DragDropEffects.Move;
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+            if (dropInfo.Data is string && dropInfo.TargetItem is string)
+            {
+                var droppedData = dropInfo.Data as string;
+                var targetData = dropInfo.TargetItem as string;
+
+                // Access FileList through the _viewModel reference
+                var index = _viewModel.FileList.IndexOf(targetData);
+
+                _viewModel.FileList.Remove(droppedData);
+                _viewModel.FileList.Insert(index, droppedData);
+            }
+        }
+    }
+}
+
+
+
